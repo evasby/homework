984f7cad57fa0f9381574389b02b420a782b7ec2
Uladzimir Dzmitryieu <Uladzimir_Dzmitryieu@epam.com>
2017-11-22 17:50:37 +0300

    3

---
+++ sites/all/modules/customs/homework/theming-template--employees.tpl.php
@@ -0,0 +1,5 @@
+<ul>
+  <li><?php print $variables['items'][0]['name']; ?></li>
+  <li><?php print $variables['items'][0]['salary']; ?></li>
+  <li><?php print $variables['items'][0]['job title']; ?></li>
+</ul>
